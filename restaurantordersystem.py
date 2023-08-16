4df617ac32bec9254da7f3df1327e356299740c1
Melih <melihcan1376@gmail.com>
2023-08-16 18:56:33 +0300

    Added Payment method and improvements.Refund, waitress_tips and invoice methods as placeholders will be updated in the future

--- restaurantordersystem.py
+++ restaurantordersystem.py
@@ -10,22 +10,23 @@ class RestaurantOrderSystem():
     def __init__(self, name):
         self.name = name
         self.menu = {
-            'Fries':         19.99,
-            'Sandwich':      24.99,
-            'Cheeseburger':  14.99,
-            'Coffee':         7.99,
-            'Soda':           4.99,
+            'Cheeseburger':  24.99,
+            'Sandwich':      19.99,
+            'Fries':         16.99,
+            'Coffee':        12.99,
+            'Soda':          10.99,
         }
 
     # Specifies customer order
     def customer(self, order):
         try:
+            self.order = order # Store the order as an attribute
             print("="*50)
-            print(f"The Menu: {order} - {self.menu[order]}$")
+            print(f"The Menu: {order} - ${self.menu[order]:.2f}")
             print("="*50)
 
         except KeyError:
-            print("Item not found in the menu.")
+            print("Please choose another item from the menu.")
 
         else:
             print(f"Thanks for your order. {self.name}")
@@ -37,7 +38,7 @@ class RestaurantOrderSystem():
         print("="*30)
         for index, food in enumerate(self.menu, start=1):
             price = self.menu[food]
-            print("{:<0}. {:<20} {:<10}".format(index, food, price))
+            print("{:<0}. {:<20} {:<5}$".format(index, food, price))
         print("="*30)
 
     # Welcome Message to Customer
@@ -46,8 +47,38 @@ class RestaurantOrderSystem():
         print("What are you eating or drinking today ?\n")
 
     # An empty payment function but this will be completed in the future
-    def payment(self):
-        pass 
+    def payment(self, payment_method, balance):
+        if payment_method == "Creditcard":
+            if balance >= self.menu[self.order]:
+                balance -= self.menu[self.order]
+                print("Payment Confirmed.")
+                print("Thank you for choosing us.")
+                print("Have a nice day!")
+            else:
+                print("Insufficient Balance!")
+                print("Please Try Again or Change Payment Method!")
+        elif payment_method == "Cash":
+            if balance >= self.menu[self.order]:
+                balance -= self.menu[self.order]
+                print("Thanks for your payment!")
+                print("Thank you for choosing us.")
+                print("Have a nice day!")
+            else:
+                print("Insufficient Balance!")
+                print("Please Try Again or Change Payment Method!")
+    
+    # Will be arranged soon for refund system
+    def refund():
+        pass    
+    
+    # Will be arranged soon for the waiter tip system
+    def waitress_tips():
+        pass
+    
+    # Will be arranged soon for the invoice system
+    def invoice():
+        pass
+
 
     def login_interface(self, username, password):
         admin = "Micheal"
@@ -66,23 +97,29 @@ class RestaurantOrderSystem():
 # Here is the Main Function
 def main():
     try:
-        print(color.LIGHTGREEN_EX)
-        print(figlet("Welcome Restaurant"))
-        greeting = RestaurantOrderSystem("")
-        print(color.YELLOW)
-        greeting.greeting()
-        customer_name = input("Hello there! What's your name ? \n\n")
-        print(color.LIGHTBLUE_EX)
-        order = RestaurantOrderSystem(customer_name)   
-        username = input("Username: ")
-        password = input("Password: ")
-        order.login_interface(username, password)
-        order.display_menu()
-        print(color.LIGHTGREEN_EX)
-        customers_order = input("Please enter order menu: ")
-        order.customer(customers_order)
-        input()
-        command("cls")
+        while True:
+            print(color.LIGHTGREEN_EX)
+            print(figlet("Welcome Restaurant"))
+            greeting = RestaurantOrderSystem("")
+            print(color.YELLOW)
+            greeting.greeting()
+            customer_name = input("Hello there! What's your name ? \n\n")
+            print(color.LIGHTBLUE_EX)
+            order = RestaurantOrderSystem(customer_name)   
+            username = input("Username: ")
+            password = input("Password: ")
+            order.login_interface(username, password)
+            order.display_menu()
+            print(color.LIGHTGREEN_EX)
+            customers_order = input("Please enter order menu: ").capitalize()
+            order.customer(customers_order)
+            customer_payment = input("How would you like to make your payment ? ").capitalize()
+            customer_balance = float(input("Enter the amount to pay: "))
+            order.payment(customer_payment,customer_balance)
+            exit_question = input("Exit (Q): ").upper()
+            if exit_question == "Q":
+                break
+            command("cls")
 
     except Exception as e:
         print(e)
