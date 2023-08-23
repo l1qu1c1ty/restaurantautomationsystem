c0549cfed72e82a1a9a4bbcd1d7eea5d5ab931de
Melih <melihcan1376@gmail.com>
2023-08-23 17:24:40 +0300

    Updated RestaurantOrderSystem class and improved user interactions

--- restaurantordersystem.py
+++ restaurantordersystem.py
@@ -1,54 +1,58 @@
-# Restaurant Order and Payment Automation
+# Importing necessary libraries
 from pyfiglet import figlet_format as figlet
 from colorama import Fore as color
 from os import system as command
 import sqlite3 as sql
 
+# Restaurant Order and Payment Automation
 class RestaurantOrderSystem():
     def __init__(self):
+        # Initialize menu dictionary and database connection variables
         self.menu = {}
         self.connection = None
         self.c = None
     
     def setup_database(self):
         try:
+            # Connect to the database and fetch menu data
             self.connection = sql.connect("restaurant.db")
             self.c = self.connection.cursor()
             self.c.execute("SELECT name, price FROM menu")
             self.menu_data = self.c.fetchall()
 
+            # Populate menu dictionary with menu items and prices
             for name, price in self.menu_data:
                 self.menu[name] = price
         
         except sql.Error as e:
             print("An error occurred while fetching the menu:", e)
+        
+        except Exception as e:
+            print("An error occurred:", e)
 
-    def customer(self):
+    def customer_menu(self):
         while True:
+            # Get customer's order input and validate it against the menu
             self.customer_order = input("Please enter order menu: ").capitalize()
             cleaned_order = self.customer_order.strip().lower()
             
-            price = None
             for menu_item, item_price in self.menu.items():
                 if cleaned_order == menu_item.lower():
-                    price = item_price
-                    break
-
-            if price is not None:
-                self.order_price = price
-                print("=" * 50)
-                print("The Menu: {} - ${}".format(self.customer_order, price))
-                print("=" * 50)
-                print("Thanks for your order, {}!".format(self.name))
-                print("Your order will be ready in ~10 minutes.")
-                print("=" * 50)
-                break
+                    self.order_price = item_price
+                    print("=" * 50)
+                    print("The Menu: {} - ${}".format(self.customer_order, item_price))
+                    print("=" * 50)
+                    print("Thanks for your order, {}!".format(self.name))
+                    print("Your order will be ready in ~10 minutes.")
+                    print("=" * 50)
+                    return
             
             else:
                 print("Invalid menu. Please choose another item from the menu.")
 
     def display_menu(self):
         try:
+            # Display the menu items and prices
             print("=" * 30)
             for index, (name, price) in enumerate(self.menu_data, start=1):
                 print("{:<0}. {:<20} {:<5}$".format(index, name, price))
@@ -57,42 +61,67 @@ class RestaurantOrderSystem():
         except sql.Error as e:
             print("An error occurred:", e)
 
-    # Welcome Message to Customer
-    def greeting(self):
+    def salutation_customer(self):
+        # Welcome and ask for the customer's name
         print("Welcome Restaurant!\n")
         print("What are you eating or drinking today ?\n")
         self.name = input("Hello there! What is your name?\n\n")
 
-    def payment(self):
+    def payment_process(self):
         while True:
-            self.payment_method = input("Cash or Creditcard:").capitalize()
-            if self.payment_method == "Creditcard" or self.payment_method == "Cash":
-                self.customer_balance = float(input("Enter the amount to pay $"))
-                prices = float(self.menu[self.customer_order])
-                if self.customer_balance >= prices:
-                    print("Payment Confirmed.")
-                    break
+            try:
+                # Get payment method and validate it
+                self.payment_method = input("Cash or Creditcard:").capitalize()
+                if self.payment_method == "Creditcard" or self.payment_method == "Cash":
+                    self.customer_balance = float(input("Enter the amount to pay $"))
+                    prices = float(self.menu[self.customer_order])
+                    if self.customer_balance >= prices:
+                        print("Payment Confirmed.")
+                        break
+                    else:
+                        print("Insufficient Balance!")
+                        print("Please Try Again or Change Payment Method!")
+                        break
                 else:
-                    print("Insufficient Balance!")
-                    print("Please Try Again or Change Payment Method!")
-                    break   
-            else:
-                print("Invalid Payment Method!")
-
-    def waitress_tips(self):
-        self.customer_tip = float(input("Would you like to tip $"))
-        print("-"*50)
-        if self.customer_balance >= float(self.order_price) + self.customer_tip and self.customer_tip >= 0:
-            print(f"{self.name} Thanks for the tips.")
-            print(f"Tip: ${self.customer_tip:.2f}")
-            print("Thank you for choosing us.")
-            print("Have a nice day!")
-        else:
-            print("Balance is not enough to pay the tip.")
-            print(f"Tip is invalid: ${self.customer_tip:.2f}")
-        print("-"*50)
+                    print("Invalid Payment Method!")
+                
+            except ValueError:
+                print("\n")
+                print("*"*50)
+                print("Please enter a numeric value")
+                print("*"*50)
+        
+            except KeyboardInterrupt:
+                print("\n")
+                print("*"*50)
+                print("Exiting the Program...")
+                print("*"*50)
+                exit()
+            
+    def waitress_tip(self):
+        while True:
+            try:
+                # Get tip amount and validate it
+                self.customer_tip = float(input("Would you like to tip $"))
+                print("-"*50)
+                
+                if self.customer_balance >= float(self.order_price) + self.customer_tip and self.customer_tip >= 0:
+                    print(f"{self.name} Thanks for the tips.")
+                    print(f"Tip: ${self.customer_tip:.2f}")
+                    print("Thank you for choosing us.")
+                    print("Have a nice day!")
+                    break
+                
+                else:
+                    print("Balance is not enough to pay the tip.")
+                    print(f"Tip is invalid: ${self.customer_tip:.2f}")
+                print("-"*50)
+        
+            except ValueError:
+                print("Please enter a numeric value")
     
     def invoice(self):
+        # It gives the price of the menu, the amount paid, tip, change and payment method information.
         self.change = self.customer_balance - float(self.order_price) - self.customer_tip
         if self.change >= 0:
             separator = "-" * 50
@@ -118,6 +147,8 @@ class RestaurantOrderSystem():
     
     def login_interface(self):
         try:
+            # It allows the user to log in. If the user exists in the database, Its log in. 
+            # Otherwise, he continues in the guest session.
             self.username = input("Username: ")
             self.password = input("Password: ")
             self.c.execute("SELECT username, password FROM login WHERE username = ?", (self.username,))
@@ -147,22 +178,20 @@ def main():
             command("cls")
             print(color.LIGHTGREEN_EX)
             print(figlet("Welcome Restaurant"))    
-            
-            print(color.LIGHTBLUE_EX)
             order = RestaurantOrderSystem()
             order.setup_database()
             
             print(color.YELLOW)
-            order.greeting()
+            order.salutation_customer()
             order.login_interface()
             
             print(color.LIGHTGREEN_EX)
             order.display_menu()
-            order.customer()
-            order.payment()
-            order.waitress_tips()
+            order.customer_menu()
+            order.payment_process()
+            order.waitress_tip()
             
-            print(color.LIGHTWHITE_EX)
+            print(color.YELLOW)
             order.invoice()
             
             exit_question = input("Exit (Q): ").upper()
